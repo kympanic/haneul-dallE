ae47e88000d4f9e54a8c472d120b48e3d574f0a8
Daniel Yoo <dacapo0509@gmail.com>
2024-03-02 14:21:18 -0800

    functionality for generating image from openai api working

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -17,19 +17,19 @@ router.route("/").get((req, res) => {
 router.route("/").post(async (req, res) => {
 	try {
 		const { prompt } = req.body;
-		const aiResponse = await openai.createImage({
+		const aiResponse = await openai.images.generate({
+			model: "dall-e-3",
 			prompt,
 			n: 1,
 			size: "1024x1024",
 			response_format: "b64_json",
 		});
-
-		const image = aiResponse.data.data[0].b64_json;
-
+		const image = aiResponse.data[0].b64_json;
+		// const image = aiResponse.data.data[0].b64_json;
 		res.status(200).json({ photo: image });
 	} catch (error) {
-		console.log(error);
-		res.status(500).send(error?.response.data.error.message);
+		console.error(error);
+		res.status(500).send(error?.message);
 	}
 });
 
